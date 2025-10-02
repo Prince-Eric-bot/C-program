735a7f5c4827617167a00130a6d317b2758ec3e7
Prince Eric <princeeric848@gmail.com>
2025-10-02 10:47:21 -0400

    Add files via upload

---
+++ calculate water bill.c
@@ -0,0 +1,29 @@
+/*
+Name: ERICK MBURU NJOGU
+Reg No: PA106/G/28729/25
+Description: to calculate water bill
+*/
+#include <stdio.h>
+
+int main() {
+    int units;
+    float bill;
+
+    // Prompt the user
+    printf("Enter water units consumed: ");
+    scanf("%d", &units);
+
+    // Calculate the bill using if-else statements
+    if (units <= 30) {
+        bill = units * 20.0;
+    } else if (units <= 60) {
+        bill = (30 * 20.0) + ((units - 30) * 25.0);
+    } else {
+        bill = (30 * 20.0) + (30 * 25.0) + ((units - 60) * 30.0);
+    }
+
+    // Output the bill with 2 decimal places
+    printf("Total water bill: %.2f KES\n", bill);
+
+    return 0;
+}
