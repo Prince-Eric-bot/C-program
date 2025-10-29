895b2465f4737ab1bb9d42f1f9d28ee54c6a1b8a
Prince Eric <princeeric848@gmail.com>
2025-10-29 16:22:43 +0300

    Add fare calculation program in C
    This program calculates the fare based on the distance traveled in kilometers.

---
+++ programtocalculatefare.c
@@ -0,0 +1,27 @@
+/*
+NAME:Erick Mburu
+REG NO:PA106/G/28729/25
+DEFINITION:A programto calculate fare
+*/
+
+#include<stdio.h>
+
+int calculate_fare(int distance);
+
+int main(){
+	int distance;
+	
+	printf("Enter distance traveled(in KM):");
+	scanf("%d", &distance);
+	
+	printf("Total fare is:KSh. %.2f\n",calculate_fare(distance));
+	
+	return 0;
+}
+int calculate_fare(int distance){
+	int fare;
+	fare = distance*10;
+	
+	return fare;
+	
+}
